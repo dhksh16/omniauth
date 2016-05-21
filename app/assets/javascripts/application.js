ced2f9e4ac28e1505ae40aeb69b1746fca305fad
dhksh16 <dhksh16@daum.net>
2016-05-21 06:25:28 +0000

    first commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+// Loads all Bootstrap javascripts
+//= require bootstrap
+//= require bootstrap/scrollspy
+//= require bootstrap/modal
+//= require bootstrap/dropdown
